fabd119aea08913b701428c276e3e6ece348c06d
Stefan Kupresak <stefan_vg@hotmail.com>
2018-12-26 21:53:24 +0100

    Add subdomain endpoint implementation

--- lib/ex_trails/v1/domain.ex
+++ lib/ex_trails/v1/domain.ex
@@ -1,11 +1,26 @@
 defmodule ExTrails.V1.Domain do
   import ExTrails
   alias ExTrails.V1.Client
+  alias ExTrails.V1.Domain.DomainInfo
+  alias ExTrails.V1.Domain.Subdomains
 
   @doc """
   Wrapper function for: https://api.securitytrails.com/v1/domain/(hostname)
+
+  Returns the DNS records associated with the hostname.
+  ( See documentation at https://docs.securitytrails.com/v1.0/reference#get-domain )
   """
   def info(%Client{} = client, hostname) do
-    req_get(client, "/domain/#{hostname}", ExTrails.V1.Domain.DomainInfo)
+    req_get(client, "/domain/#{hostname}", DomainInfo)
+  end
+
+  @doc """
+  Wrapper function for: https://api.securitytrails.com/v1/domain/(hostname)/subdomains
+
+  Returns the first 2000 domains of a given hostname as a list.
+  ( See documentation at https://docs.securitytrails.com/v1.0/reference#list-subdomains )
+  """
+  def subdomains(%Client{} = client, hostname) do
+    req_get(client, "/domain/#{hostname}/subdomains", Subdomains)
   end
 end
---
+++ lib/ex_trails/v1/domain/subdomains.ex
@@ -0,0 +1,6 @@
+defmodule ExTrails.V1.Domain.Subdomains do
+  defstruct [
+    :subdomains,
+    :endpoint
+  ]
+end
